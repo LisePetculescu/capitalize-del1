a50eb6507e6512c9e49e4b63ca4a3a29e6384adb
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-13 19:14:31 +0100

    gg

--- capitalize.js
+++ capitalize.js
@@ -1,1 +1,13 @@
 "use-strict";
+
+const myName = "lISe";
+console.log(myName)
+
+const capFirst = myName.substring(0,1).toUpperCase();
+console.log(capFirst);
+
+const smallBefore = myName.substring(1).toLowerCase();
+console.log(smallBefore);
+
+const newName = `${capFirst}${smallBefore}`
+console.log(newName);
