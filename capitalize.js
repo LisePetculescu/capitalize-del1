de69d81275623d5e6a192c095b85980771c8dc0b
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-16 11:23:46 +0100

    jj

--- capitalize.js
+++ capitalize.js
@@ -1,13 +1,19 @@
 "use-strict";
 
-const myName = "lISe";
-console.log(myName);
+// const myName = "lISe";
+// console.log(myName);
 
-const capFirst = myName.substring(0, 1).toUpperCase();
-console.log(capFirst);
+function capitalized(myName) {
+  const capFirst = myName.substring(0, 1).toUpperCase();
+  console.log(capFirst);
 
-const smallAfter = myName.substring(1).toLowerCase();
-console.log(smallAfter);
+  const smallAfter = myName.substring(1).toLowerCase();
+  console.log(smallAfter);
 
-const newName = `${capFirst}${smallAfter}`;
-console.log(newName);
+  const newName = `${capFirst}${smallAfter}`;
+  console.log(newName);
+
+  return newName;
+}
+
+capitalized("lISe");
